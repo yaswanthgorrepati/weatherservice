bd7fd204499ce58a231026da0b8afb62da2ee9ea
yaswanthgorrepati <51254169+yaswanthgorrepati@users.noreply.github.com>
2021-11-15 08:38:04 +0530

    first commit

---
+++ src/test/java/com/example/weatherservice/WeatherserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
